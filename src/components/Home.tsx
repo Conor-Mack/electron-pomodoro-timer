9296e436d5a3e5081dc321d5e53ae6e13fefa107
Conor McKeown <conor.mckeown@blue-zinc.com>
2019-05-24 15:12:33 +0100

    first push

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -21,9 +21,25 @@ export default class Home extends React.Component<undefined, undefined> {
   render() {
     return (
       <div className="test">
-        <h2>Welcome to React with Typescript! {store.counter}</h2>
-        <button onClick={() => store.increment()}>hello</button>
-        <Link to="/help">More Info</Link>
+        <svg>
+          <circle
+            cx="150"
+            cy="150"
+            r="90"
+            fill="none"
+            stroke-dasharray="565"
+            stroke-dashoffset="0"
+          />
+          <circle
+            id="progress-circle"
+            class="prog-circ"
+            cx="150"
+            cy="150"
+            r="90"
+            fill="none"
+          />
+          <text id="time-label" x="105" y="160" class="small" />
+        </svg>
       </div>
     );
   }
